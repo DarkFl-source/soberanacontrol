b6f9dc640f7b9c5dd0a952c7d1ed9d7be89aa6a7
FabianoLisboas <fabianolisboa@gmail.com>
2026-05-12 23:17:27 -0300

    feat: Mobile-first responsive sidebar layout

--- frontend/src/app/layout/base-layout/base-layout.component.ts
+++ frontend/src/app/layout/base-layout/base-layout.component.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-base-layout',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [CommonModule, RouterOutlet, SidebarComponent],
   templateUrl: './base-layout.component.html',
   styleUrl: './base-layout.component.css'
 })
 export class BaseLayoutComponent {
+  isMobileMenuOpen = false;
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
 }
